78ec58fd9c0f682bb9752f9367f38230b9d0e50f
Charlotte Lafage <lafage.cha@gmail.com>
2020-07-23 14:48:21 +0200

    [#92] Try to fix email for online.

--- application.py
+++ application.py
@@ -74,9 +74,9 @@ headers = { 'Content-Type': 'application/json', 'trakt-api-key': '9cba8155f5e9c9
 # Configure mail
 # TODO: store credentials in db or somewhere else that's safe
 app.config["MAIL_SERVER"] = 'mail.privateemail.com'
-app.config["MAIL_USERNAME"] = os.getenv("MAIL_USERNAME")
-app.config["MAIL_DEFAULT_SENDER"] = os.getenv("MAIL_USERNAME")
-app.config["MAIL_PASSWORD"] = os.getenv("MAIL_PASSWORD")
+app.config["MAIL_USERNAME"] = 'OsgaLotta37'
+app.config["MAIL_DEFAULT_SENDER"] = 'staff@osga-cemetery.com'
+app.config["MAIL_PASSWORD"] = 'staff@osga-cemetery.com'
 app.config["MAIL_PORT"] = 587
 app.config["MAIL_USE_TLS"] = True
 app.config["MAIL_USE_SSL"] = False
