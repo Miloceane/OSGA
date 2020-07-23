cb9ead5c7475e05ff7766fda55925e57cd51e58a
Charlotte Lafage <lafage.cha@gmail.com>
2020-07-23 14:59:03 +0200

    [#92] Try to fix email for online.

--- application.py
+++ application.py
@@ -77,9 +77,9 @@ app.config["MAIL_SERVER"] = 'mail.privateemail.com'
 app.config["MAIL_USERNAME"] = 'OsgaLotta37'
 app.config["MAIL_DEFAULT_SENDER"] = 'staff@osga-cemetery.com'
 app.config["MAIL_PASSWORD"] = 'staff@osga-cemetery.com'
-app.config["MAIL_PORT"] = 587
-app.config["MAIL_USE_TLS"] = True
-app.config["MAIL_USE_SSL"] = False
+app.config["MAIL_PORT"] = 465
+app.config["MAIL_USE_TLS"] = False
+app.config["MAIL_USE_SSL"] = True
 app.config["MAIL_DEBUG"] = True
 mail = Mail(app)
 
